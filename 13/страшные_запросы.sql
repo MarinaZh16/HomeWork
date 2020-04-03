aad580c8a9c7ee157efc95e99e5b41c185b2a4fb
Mary <msahno16@gmail.com>
2020-04-04 02:26:26 +0300

    h_w_13

--- 13/страшные_запросы.sql
+++ 13/страшные_запросы.sql
@@ -1,25 +1,25 @@
-﻿select concat( a.name, ' ', a.surname, ' starred in the movie "',
+﻿1) select concat( a.name, ' ', a.surname, ' starred in the movie "',
 f.title, '", directed by ', p.name, ' ', p.surname) as info from
 actor_film a_f inner join producer_film p_f using(film_id) join actor a
 using(actor_id) join film f using(film_id) join producer p
 using(producer_id);
 ================================================================================
-select film.title, count(film.title) from actor_film join actor
+2) select film.title, count(film.title) from actor_film join actor
 using(actor_id) join film using(film_id) group by film.title;
 ================================================================================
-select concat(a.name, ' ', a.surname) as actor, f.title as film,
+3) select concat(a.name, ' ', a.surname) as actor, f.title as film,
 concat(p.name, ' ', p.surname) as producer from actor_film a_f inner
 join producer_film p_f using(film_id) join actor a using(actor_id) join
 film f using(film_id) join producer p using(producer_id);
 ================================================================================
-select f.title, concat(p.name, ' ', p.surname) as producer,
+4) select f.title, concat(p.name, ' ', p.surname) as producer,
 string_agg(cast(' ' as varchar(500)), concat(a.name, ' ', a.surname,
 ',')) as actors from actor_film a_f inner join producer_film p_f
 using(film_id) join actor a using(actor_id) join film f using(film_id)
 join producer p using(producer_id) where film_id=1 group by
 p.producer_id, f.film_id;
 ================================================================================
-select concat(' Фильм ', '"', f.title, '"', ' был снят режиссёром по
+5) select concat(' Фильм ', '"', f.title, '"', ' был снят режиссёром по
 имени ', p.name, ' ', p.surname, '. В нём снимались: ',
 string_agg(cast(' ' as varchar(500)), concat(a.name, ' ', a.surname,
 ',')), 'и другие.') from actor_film a_f inner join producer_film p_f
@@ -27,6 +27,26 @@ using(film_id) join actor a using(actor_id) join film f using(film_id)
 join producer p using(producer_id) where film_id=1 group by
 p.producer_id, f.film_id;
 ================================================================================
+в 4 и 5 запросах выводятся не все актёры почему-то( но, если объединить
+имя и фамилию в один столбец, то запрос будет чуть проще, из
+string_agg(cast(' ' as varchar(500)), concat(a.name, ' ', a.surname,
+',')) можно убрать cast(' ' as varchar(500)), concat(.....) и всё
+работает, никто не теряется:
+================================================================================
+6) alter table actor add column actor_name text;
 
+update actor set actor_name=concat(name, ' ', surname);
 
+select concat(' Фильм ', '"', f.title, '"', ' был снят режиссёром по
+имени ', p.name, ' ', p.surname, '. В нём снимались: ',
+string_agg(a.actor_name, ', '), ' и другие.') from actor_film a_f inner
+join producer_film p_f using(film_id) inner join actor a using(actor_id)
+inner join film f using(film_id) inner join producer p
+using(producer_id) where film_id=3 group by p.producer_id, f.film_id;
 
+7) select f.title, concat(p.name, ' ', p.surname) as producer,
+string_agg(a.actor_name,
+',') as actors from actor_film a_f inner join producer_film p_f
+using(film_id) join actor a using(actor_id) join film f using(film_id)
+join producer p using(producer_id) where film_id=1 group by
+p.producer_id, f.film_id;
