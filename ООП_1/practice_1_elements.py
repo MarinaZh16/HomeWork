bfc366add35661b00cd368ee16ad6b4453d47e7d
Mary <msahno16@gmail.com>
2020-04-09 20:01:02 +0300

    Lect_15_ООП(1)

---
+++ ООП_1/practice_1_elements.py
@@ -0,0 +1,24 @@
+class Element():
+    def __init__(self, melt_temp, vapor_temp):
+        self.melt_temp=melt_temp
+        self.vapor_temp=vapor_temp
+    def convert_to_c(self, temp, scale):
+        if scale=="K":
+            temp=temp-273
+        if scale=="F":
+            temp=temp*9/5+32
+        return temp
+        
+    def agg_state(self, temp, scale="C"):
+        temp=self.convert_to_c(temp, scale)            
+        if temp < self.melt_temp:
+            return "SOLID", temp
+        if temp >= self.vapor_temp:
+            return "GAS", temp
+        return "LIQUID", temp
+    
+
+oxygen=Element(melt_temp=-218, vapor_temp=-182)
+iron=Element(melt_temp=1538, vapor_temp=2862)
+print(oxygen.agg_state(-190, "K"))
+print(iron.agg_state(2000, "K"))
---
+++ ООП_1/task_1_products.py
@@ -0,0 +1,45 @@
+class Product():
+    def __init__(self, price, category, name, unit):
+        self.price=price
+        self.category=category
+        self.name=name
+        self.unit=unit
+    def is_eatable(self):
+        if self.category=="household chemicals":
+            return False
+        return True
+    
+    def price_total(self, quantity):
+        return quantity*self.price
+
+    
+class Basket():
+    def __init__(self, buy_list):
+        self.buy_list=buy_list
+    
+    def total(self):
+        total_sum=0
+        for p, q in self.buy_list:
+            total_sum+=p.price_total(q)
+        return total_sum
+    
+        
+    def totally_eatable(self):
+        for i in self.buy_list:
+            if i[0].is_eatable()==False:
+                return ('Не все продукты съедобны!')
+        return ('Все продукты съедобны!')
+                
+        
+
+bread=Product(price=10, category='food', name='bread', unit='шт.')
+shampoo=Product(price=150, category='household chemicals', name='Nivea', unit='шт.')
+cheese=Product(price=50, category='food', name='Maasdamer', unit='кг.')
+bananas=Product(price=25, category='food', name='banana', unit='кг')
+milk=Product(price=28.90, category='food', name='Ферми', unit='л')
+wine=Product(price=150, category='alcohol', name='VinishKo', unit='бут.')
+b=Basket([[bananas, 1.5], [bread, 3], [cheese, 0.3], [shampoo, 2], [milk, 1], [wine, 3]])
+print('Общая сумма покупки = %s грн.' % b.total())
+print(b.totally_eatable())
+
+
