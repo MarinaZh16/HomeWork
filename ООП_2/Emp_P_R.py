85abf0ead8e489395243459fbd1b56c790d24884
Mary <msahno16@gmail.com>
2020-04-16 23:41:55 +0300

    Lect_16_ООП(2)

--- ООП_2/Emp_P_R.py
+++ ООП_2/Emp_P_R.py
@@ -1,27 +1,26 @@
-from datetime import date, timedelta
-now=date.today()
-month_start=date(now.year, now.month, 1)
-weekend=[5, 6]
-diff=(now - month_start).days+1
-day_count=0
-for day in range(diff):
-    if(month_start + timedelta(day)).weekday() not in weekend:
-        day_count+=1
-
-            
 class Employee:
     def __init__(self, name, surname, email, phone_number, salary):
-        self.name=name
-        self.surname=surname
-        self.email=email
-        self.phone_number=phone_number
-        self.salary=salary
-    
+        self.name = name
+        self.surname = surname
+        self.email = email
+        self.phone_number = phone_number
+        self.salary = salary
+    from datetime import date, timedelta
+    now = date.today()
+    month_start = date(now.year, now.month, 1)
+    weekend = [5, 6]
+    diff = (now - month_start).days + 1
+    day_count = 0
+    for day in range(diff):
+        if(month_start + timedelta(day)).weekday() not in weekend:
+            day_count += 1
+
     def work(self):
         return 'I come to the office.'
         
     def check_salary(self, days=day_count):
-        return '%s $' %(self.salary*days)
+       
+        return '%s $' %(self.salary * days)
 
     def __lt__(self, other):
         return 'LT: %s' %(self.salary < other.salary) 
@@ -47,57 +46,50 @@ class Employee:
 
 class Recruiter(Employee):
     def __init__(self, name, surname, email, phone_number, salary,
-                 hired_this_monht):
+                 hired_this_month):
         super().__init__(name, surname, email, phone_number, salary)
-        self.hired_this_monht=hired_this_monht
+        self.hired_this_month = hired_this_month
 
-    def full_salary(self):
-        month_start=date(now.year, self.hired_this_monht, 1)
-        diff=(now - month_start).days+1
-        day_count=0
-        for day in range(diff):
-            if(month_start + timedelta(day)).weekday() not in weekend:
-                day_count+=1        
-        return 'Full salary = %s $' %(self.salary*day_count)
-        
     def work(self):
-        emp_work=super().work()[:-1]
-        return emp_work+' and start hiring'
+        emp_work = super().work()[:-1]
+        return emp_work + ' and start hiring'
+
 
 class Programmer(Employee):
     def __init__(self, name, surname, email, phone_number, salary, tech_stack,
-                 closed_this_monht):
+                 closed_this_month):
         super().__init__(name, surname, email, phone_number, salary)
-        self.tech_stack=tech_stack
-        self.closed_this_monht=closed_this_monht
-        
+        self.tech_stack = tech_stack
+        self.closed_this_month = closed_this_month
+
     def __lt__(self, other):
         return 'Stack LT: %s' %(len(self.tech_stack) < len(other.tech_stack))  
 
     def __gt__(self, other):
-        return 'Stack GT: %s' %(len(self.tech_stack) > len(other.tech_stack)) 
-        
+        return 'Stack GT: %s' %(len(self.tech_stack) > len(other.tech_stack))
+
     def __eq__(self, other):
-        return 'Stack EQ:%s' %(len(self.tech_stack) == len(other.tech_stack)) 
+        return 'Stack EQ:%s' %(len(self.tech_stack) == len(other.tech_stack))
 
     def __le__(self, other):
-        return 'Stack LE: %s' %(len(self.tech_stack) <= len(other.tech_stack)) 
+        return 'Stack LE: %s' %(len(self.tech_stack) <= len(other.tech_stack))
 
     def __ge__(self, other):
-        return 'Stack GE:%s' %(len(self.tech_stack) >= len(other.tech_stack)) 
-    
+        return 'Stack GE:%s' %(len(self.tech_stack) >= len(other.tech_stack))
+
     def __ne__(self, other):
-        return 'Stack NE: %s' %(len(self.tech_stack) != len(other.tech_stack)) 
+        return 'Stack NE: %s' %(len(self.tech_stack) != len(other.tech_stack))
 
     def work(self):
-        emp_work=super().work()[:-1]
-        return emp_work+' and start coding'
+        emp_work = super().work()[:-1]
+        return emp_work + ' and start coding'
     
     def super_p(self, other):
-        tech_stack=self.tech_stack+other.tech_stack
-        closed_this_monht=self.closed_this_monht+other.closed_this_monht
+        tech_stack = self.tech_stack + other.tech_stack
+        closed_this_month = self.closed_this_month + other.closed_this_month
         return 'Superskills: %s ; number of closed tasks : %s'\
-               %(list(set(tech_stack)), closed_this_monht)
+               %(list(set(tech_stack)), closed_this_month)
+
 
 P1 = Programmer('Marina', 'Zhidkova', 'mzh@gmail.com', '066xxxxxxx', 22,
                 ['python', 'C++', 'Ruby', 'C'], 3)
@@ -112,7 +104,7 @@ print(str(R1))
 print(P1 < P2)
 print(str(P2), P2.check_salary())
 print(str(P2), P2.check_salary(18))
-print(str(R2), R2.full_salary())
-print(R1<R2)
-print(R1==P1)
+print(R1 < R2)
+print(R1 == P1)
 print(P1.super_p(P2))
+
